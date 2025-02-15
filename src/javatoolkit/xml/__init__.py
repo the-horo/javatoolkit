033b3fd64b9bc06e83029ef10bd10a8b0df207a6
Arthur Zamarin <arthurzam@gentoo.org>
2025-02-15 12:11:18 +0200

    remove xml-rewrite scripts
    Closes: https://bugs.gentoo.org/942810
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/javatoolkit/xml/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#
-# Copyright(c) 2007, Gentoo Foundation
-# Licensed under the GNU General Public License, v2
-
-if __name__ == "__main__":
-	print("This is not an executable module")
